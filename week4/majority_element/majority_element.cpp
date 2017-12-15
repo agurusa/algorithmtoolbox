ae34c99bb4f9849dda5bc911c117edbe687cedbb
agurusa <agurusa@gmail.com>
2017-12-15 11:59:54 -0800

    improve time limit on divide and conquer method. increase efficiency by searching through smaller vector sizes.

--- week4/majority_element/majority_element.cpp
+++ week4/majority_element/majority_element.cpp
@@ -5,7 +5,6 @@
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right){
-  std::cout << left << " left " << right << " right \n";
   if (left == right){
     return -1;
   } 
@@ -14,13 +13,11 @@ int get_majority_element(vector<int> &a, int left, int right){
   } 
   else{
     int mid = (right - left)/2;
-    std::cout << mid << " mid \n";
     int ml = get_majority_element(a, left, left + mid);
     int mr = get_majority_element(a, left + mid + 1, right);
-    std::cout << ml << " ml " << mr << " mr \n";
     if (ml == mr) return ml; 
-    else if(count(a.begin(), a.end(), ml) > mid) return ml;
-    else if(count(a.begin(), a.end(), mr) > mid) return mr;
+    else if(count(a.begin() + left, a.begin() + right, ml) > mid) return ml;
+    else if(count(a.begin() + left, a.begin() + right, mr) > mid) return mr;
     else {
       return -1;
     }
