96bc6745b9a91dc0a88cb266a73002389c015a42
agurusa <agurusa@gmail.com>
2017-12-15 11:46:40 -0800

    fix divide and conquer to prevent double counting of middle element

--- week4/majority_element/majority_element.cpp
+++ week4/majority_element/majority_element.cpp
@@ -5,6 +5,7 @@
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right){
+  std::cout << left << " left " << right << " right \n";
   if (left == right){
     return -1;
   } 
@@ -13,8 +14,10 @@ int get_majority_element(vector<int> &a, int left, int right){
   } 
   else{
     int mid = (right - left)/2;
+    std::cout << mid << " mid \n";
     int ml = get_majority_element(a, left, left + mid);
-    int mr = get_majority_element(a, left + mid, right);
+    int mr = get_majority_element(a, left + mid + 1, right);
+    std::cout << ml << " ml " << mr << " mr \n";
     if (ml == mr) return ml; 
     else if(count(a.begin(), a.end(), ml) > mid) return ml;
     else if(count(a.begin(), a.end(), mr) > mid) return mr;
