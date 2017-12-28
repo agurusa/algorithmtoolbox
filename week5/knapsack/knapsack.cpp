0184bc6eacf8311fefc479292e7177f63c40c31d
agurusa <agurusa@gmail.com>
2017-12-27 16:42:04 -0800

    Add dynamic capabilities to knapsack algorithm

--- week5/knapsack/knapsack.cpp
+++ week5/knapsack/knapsack.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using std::vector;
+
+int op_weight(int W, const vector<int> &w){
+  vector<int> item_vector = {};
+  for(int i = 0; i < w.size(); i++){
+    vector<int> copy_weight_vector = w;
+    if(w[i] < W){
+      item_vector.push_back(w[i]);
+      copy_weight_vector.erase(copy_weight_vector.begin() + i);
+      for(int y = 0; y < copy_weight_vector.size(); y ++){
+        if(copy_weight_vector[y] < W - item_vector[i]){
+          item_vector[i] += copy_weight_vector[y];
+        }
+        else{
+        }
+      }
+    }
+    std::sort(item_vector.begin(), item_vector.end());  }
+  return item_vector[item_vector.size() - 1];
+}
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
@@ -21,5 +41,6 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  // std::cout << optimal_weight(W, w) << '\n';
+  std::cout << op_weight(W,w) << '\n';
 }
